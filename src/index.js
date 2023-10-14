dfbcdf1721e3efa6d3022c2baa832aa1cd7f9fb7
Wilmer <gestiondwil@gmail.com>
2023-10-14 11:53:48 -0500

    First commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require ('express');
+const app = express(); 
+
+//Settings 
+
+//Middlewares 
+
+// Rutas 
+
+// Archivos estaticos 
+
+// iniciando servidor 
+
+app.listen(3000, () =>{
+    console.log('server on port 3000');
+});
